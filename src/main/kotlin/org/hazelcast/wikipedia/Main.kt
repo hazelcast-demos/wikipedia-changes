b859d9c858bc33a8d64f2ea64b513e3b686f34f8
Nicolas Frankel <nicolas@frankel.ch>
2021-07-01 17:48:46 +0200

    Add Elasticsearch sink

--- src/main/kotlin/org/hazelcast/wikipedia/Main.kt
+++ src/main/kotlin/org/hazelcast/wikipedia/Main.kt
@@ -2,13 +2,12 @@ package org.hazelcast.wikipedia
 
 import com.hazelcast.core.Hazelcast
 import com.hazelcast.jet.pipeline.Pipeline
-import com.hazelcast.jet.pipeline.Sinks
 
 fun main() {
     val pipeline = Pipeline.create().apply {
         readFrom(wikipedia)
             .withTimestamps({ it.getLong("timestamp") }, 100)
-            .writeTo(Sinks.logger())
+            .writeTo(elasticsearch)
     }
     Hazelcast.bootstrappedInstance().jet.newJob(pipeline)
 }
