3d7230d079f72d797548b07a19e06839b0b90e20
Nicolas Frankel <nicolas@frankel.ch>
2021-07-15 16:59:52 +0200

    Enrich with IP geolocation for anonymous contributions

--- src/main/kotlin/org/hazelcast/wikipedia/Main.kt
+++ src/main/kotlin/org/hazelcast/wikipedia/Main.kt
@@ -12,6 +12,7 @@ fun main() {
         readFrom(wikipedia)
             .withTimestamps({ it.getLong("timestamp") }, 100)
             .apply(RemoveFieldIfArray("log_params"))
+            .apply(enrichWithLocation)
             .peek()
             .writeTo(elasticsearch)
     }
