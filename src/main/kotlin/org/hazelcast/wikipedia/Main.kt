09685e1530fe68950eeef03161551684bd8b2dce
Nicolas Frankel <nicolas@frankel.ch>
2021-07-15 10:45:06 +0200

    Fix exception during ingestion
      At one point during pipeline execution, we have the following error that stops the job:
       type=mapper_parsing_exception, reason=object mapping for [log_params] tried to parse field [null] as object, but found a concrete value
    
      Reason is that most of the times, log_params is an object but sometimes, it's an empty array
      Because the index is created with log_params being an object, Elasticsearch cannot map it correctly when it's an array
      The solution is to remove the field altogether in that case

--- src/main/kotlin/org/hazelcast/wikipedia/Main.kt
+++ src/main/kotlin/org/hazelcast/wikipedia/Main.kt
@@ -1,14 +1,25 @@
 package org.hazelcast.wikipedia
 
 import com.hazelcast.core.Hazelcast
+import com.hazelcast.function.FunctionEx
 import com.hazelcast.jet.pipeline.Pipeline
+import com.hazelcast.org.json.JSONArray
+import com.hazelcast.org.json.JSONObject
 
 fun main() {
     val pipeline = Pipeline.create().apply {
         readFrom(wikipedia)
             .withTimestamps({ it.getLong("timestamp") }, 100)
+            .map(MakeFieldObjectIfArray("log_params"))
             .peek()
             .writeTo(elasticsearch)
     }
     Hazelcast.bootstrappedInstance().jet.newJob(pipeline)
+}
+
+class MakeFieldObjectIfArray(private val fieldName: String) : FunctionEx<JSONObject, JSONObject> {
+    override fun applyEx(json: JSONObject) = json.apply {
+        if (json.has(fieldName) && json.get(fieldName) is JSONArray)
+            put(fieldName, JSONObject())
+    }
 }
