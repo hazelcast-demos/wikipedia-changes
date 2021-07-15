5433754e80635a5b9ee71e8c455b700dc4f6651b
Nicolas Frankel <nicolas@frankel.ch>
2021-07-15 18:45:55 +0200

    Transform input to map the coordinates field to a GeoIP type in Elasticsearch

--- src/main/kotlin/org/hazelcast/wikipedia/EnrichWithLocation.kt
+++ src/main/kotlin/org/hazelcast/wikipedia/EnrichWithLocation.kt
@@ -2,8 +2,10 @@ package org.hazelcast.wikipedia
 
 import com.hazelcast.function.BiFunctionEx
 import com.hazelcast.jet.core.ProcessorSupplier
+import com.hazelcast.org.json.JSONArray
 import com.hazelcast.org.json.JSONObject
 import com.maxmind.geoip2.DatabaseReader
+import com.maxmind.geoip2.model.CityResponse
 import org.apache.commons.validator.routines.InetAddressValidator
 import java.net.InetAddress
 
@@ -19,8 +21,24 @@ val enrichWithLocation = object : BiFunctionEx<DatabaseReader, JSONObject, JSONO
             val user = json.getString("user")
             if (validator.isValid(user)) {
                 reader.tryCity(InetAddress.getByName(user))
-                    .ifPresent { json.put("location", JSONObject(it.toJson())) }
+                    .ifPresent { json.withLocationFrom(it) }
             }
         }
     }
+}
+
+private fun JSONObject.withLocationFrom(response: CityResponse) {
+    val country = JSONObject()
+        .put("iso", response.country.isoCode)
+        .put("name", response.country.name)
+    val coordinates = JSONArray()
+        .put(response.location.latitude)
+        .put(response.location.longitude)
+    val location = JSONObject()
+        .put("country", country)
+        .put("coordinates", coordinates)
+        .put("city", response.city.name)
+        .put("timezone", response.location.timeZone)
+        .put("accuracy-radius", response.location.accuracyRadius)
+    put("location", location)
 }
