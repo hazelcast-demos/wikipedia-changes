91c92124e3d2b77021bab874277b3b6390cd6ad6
Nicolas Frankel <nicolas@frankel.ch>
2021-07-20 12:03:37 +0200

    Encapsulate mapping pipeline step for better readability

--- src/main/kotlin/org/hazelcast/wikipedia/Main.kt
+++ src/main/kotlin/org/hazelcast/wikipedia/Main.kt
@@ -3,6 +3,7 @@ package org.hazelcast.wikipedia
 import com.hazelcast.core.Hazelcast
 import com.hazelcast.function.FunctionEx
 import com.hazelcast.jet.pipeline.Pipeline
+import com.hazelcast.jet.pipeline.StreamStage
 import com.hazelcast.org.json.JSONArray
 import com.hazelcast.org.json.JSONObject
 
@@ -10,16 +11,20 @@ fun main() {
     val pipeline = Pipeline.create().apply {
         readFrom(wikipedia)
             .withTimestamps({ it.getLong("timestamp") }, 100)
-            .map(MakeFieldObjectIfArray("log_params"))
+            .apply(MakeFieldObjectIfArray("log_params"))
             .peek()
             .writeTo(elasticsearch)
     }
     Hazelcast.bootstrappedInstance().jet.newJob(pipeline)
 }
 
-class MakeFieldObjectIfArray(private val fieldName: String) : FunctionEx<JSONObject, JSONObject> {
-    override fun applyEx(json: JSONObject) = json.apply {
-        if (json.has(fieldName) && json.get(fieldName) is JSONArray)
-            put(fieldName, JSONObject())
-    }
+class MakeFieldObjectIfArray(private val fieldName: String) : FunctionEx<StreamStage<JSONObject>, StreamStage<JSONObject>> {
+    override fun applyEx(stage: StreamStage<JSONObject>) = stage
+        .setName("replace-log-params-if-array")
+        .map { json ->
+            json.apply {
+                if (json.has(fieldName) && json.get(fieldName) is JSONArray)
+                    put(fieldName, JSONObject())
+            }
+        }
 }
